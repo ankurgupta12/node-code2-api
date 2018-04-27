a5f8add441f420bc954632319b27fca319040157
Ankur Gupta <ankrgpta.bhpr@gmail.com>
2018-04-27 12:22:39 +0530

    add parameter url

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var {ObjectID} = require('mongodb');
 var bodyParser = require('body-parser');
 var mongoos = require('./../mongoose');
 
@@ -49,6 +50,21 @@ app.get('/todos',(req,res)=>{
 	},(e)=>{
 		res.status(400).send(e);
 	});
+});
+app.get('/todos/:id',(req,res)=>{
+	var id = req.params.id;
+	if(!ObjectID.isValid(id)){
+	return res.status(404).send({message:'Id is not valid'});
+}
+todo.findById(id).then((todo)=>{
+if(!todo){
+	return res.status(404).send({message:'there is no data in db'});
+}
+return res.status(200).send({todo:todo});
+}).catch((e)=>{
+	res.status(404).send({message:"error in server"});
+});
+
 });
 
 app.listen('3000', () => {
