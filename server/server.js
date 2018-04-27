9ed0e85705db4d987bd5d80c046e48c2b7317ebc
Ankur Gupta <ankrgpta.bhpr@gmail.com>
2018-04-27 20:57:57 +0530

    there is update method from patch

--- server/server.js
+++ server/server.js
@@ -1,11 +1,12 @@
 var express = require('express');
-var {ObjectID} = require('mongodb');
+var _lodash= require('lodash');
+var { ObjectID } = require('mongodb');
 var bodyParser = require('body-parser');
 var mongoos = require('./../mongoose');
 
 
 var mongoos = require('mongoose');
-var port = process.env.port|| 3000;
+var port = process.env.port || 3000;
 // var todo = mongoos.model('todo',{
 // 	text:{
 // type:String,
@@ -30,9 +31,9 @@ app.use(bodyParser.json());
 
 // post method 
 app.post('/todos', (req, res) => {
-	var todo = new todo({
-    text: ""
-});
+    var todo = new todo({
+        text: ""
+    });
     console.log(req.body);
     todo.text = req.body.text;
     todo.save().then((doc) => {
@@ -45,47 +46,73 @@ app.post('/todos', (req, res) => {
 });
 
 // get method for todo 
-app.get('/todos',(req,res)=>{
-	todo.find().then((todos)=>{
-		res.status(200).send({TodoData:todos})
-	},(e)=>{
-		res.status(400).send(e);
-	});
+app.get('/todos', (req, res) => {
+    todo.find().then((todos) => {
+        res.status(200).send({ TodoData: todos })
+    }, (e) => {
+        res.status(400).send(e);
+    });
 });
-app.get('/todos/:id',(req,res)=>{
-	var id = req.params.id;
-	if(!ObjectID.isValid(id)){
-	return res.status(404).send({message:'Id is not valid'});
-}
-todo.findById(id).then((todo)=>{
-if(!todo){
-	return res.status(404).send({message:'there is no data in db'});
-}
-return res.status(200).send({todo:todo});
-}).catch((e)=>{
-	res.status(404).send({message:"error in server"});
-});
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send({ message: 'Id is not valid' });
+    }
+    todo.findById(id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send({ message: 'there is no data in db' });
+        }
+        return res.status(200).send({ todo: todo });
+    }).catch((e) => {
+        res.status(404).send({ message: "error in server" });
+    });
 
 });
 
 
-app.delete('/todos/:id',(req,res)=>{
+app.delete('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send({ message: 'id is not valid' });
+    }
+    todo.findByIdAndRemove(id).then((result) => {
+        if (!result) {
+            return res.status(404).send({ message: "there is no data For delete" });
+        }
+        return res.status(200).send({ message: "successfully Deleted!..." });
+    }).catch((e) => {
+        res.status(404).send({ message: 'Server Error' });
+    });
+});
+
+
+
+app.patch('/todos/:id',(req,res)=>{
 var id = req.params.id;
-if(!ObjectID.isValid(id)){
-return res.status(404).send({message:'id is not valid'});
-}
-todo.findByIdAndRemove(id).then((result)=>{
-if(!result){
-return res.status(404).send({message:"there is no data For delete"});
-}	
-return res.status(200).send({message:"successfully Deleted!..."});
-}).catch((e)=>{
-res.status(404).send({message:'Server Error'});
-});
+var body = _lodash.pick(req.body,['text','completed']);
+ if (!ObjectID.isValid(id)) {
+        return res.status(404).send({ message: 'id is not valid' });
+    }
+    if(_lodash.isBoolean(body.completed) && body.completed){
+
+    	body.completedAt = new Date().getTime();
+    }else{
+    	body.completedAt = null;
+    	body.completed = false;
+
+    }
+    todo.findByIdAndUpdate(id,{$set:body},{new:true} ).then((todo)=>{
+    	if(!todo){
+    		return res.status(404).send({mess:"there is no Update"})
+    	}
+    	return res.status(200).send({mess:"updated successfully!..."});
+    }).catch((e)=>{
+    	res.status(404).send({mess:"there is error"});
+    });
 });
 
 app.listen(port, () => {
-    console.log('started on port'+port);
+    console.log('started on port' + port);
 })
 
 module.exports = {
